433d212f798ca484b6f9df995b5ae1fd5770e486
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-08-17 18:39:54 +0500

    Update

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const client = new MongoClient(mongoUri);
 let db, usersCollection, videosCollection;
 
 let adminId = [907402803, 6351614390];
-const channelUsername = "t.me/+QU2m9AQSgVswYzYy";
+const channelUsername = "@+QU2m9AQSgVswYzYy";
 
 let adminStep = {
   stage: null,
